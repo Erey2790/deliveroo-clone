d8085e82f2c16fd6072cd8c0882605957a6543e8
ElmerPC <erey2790@gmail.com>
2023-08-01 16:42:36 -0500

    more icons added to header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import { View, Text, SafeAreaView, Image } from "react-native"
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from "@react-navigation/native"
-import { ChevronDownIcon } from 'react-native-heroicons/solid'
+import { ChevronDownIcon, UserIcon } from 'react-native-heroicons/solid'
 
 
 const HomeScreen = () => {
@@ -14,7 +14,7 @@ const HomeScreen = () => {
   }, [])
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
     <Text>
       {/* Header */}
       <View className="flex-row pb-3 items-center mx-4 space-x-2">
@@ -25,10 +25,12 @@ const HomeScreen = () => {
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
 
-          <View>
+          <View className="flex-1">
             <Text className="font-bold text-gray-400 text-xs">Deliver now!</Text>
-            <Text className="font-bold text-xl ">Current Location<ChevronDownIcon /></Text>
+            <Text className="font-bold text-xl ">Current Location<ChevronDownIcon size={20} color="#00CCBB"/></Text>
           </View>
+
+          <UserIcon size={35} color="#00CCBB"/>
       </View>
     </Text>
     </SafeAreaView>
