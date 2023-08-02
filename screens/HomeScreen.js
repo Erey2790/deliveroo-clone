1d4a625744c2a6e32ab5f0c2643355a4247fb61d
ElmerPC <erey2790@gmail.com>
2023-08-02 14:55:01 -0500

    fix the text sizing issue

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
-import { View, Text, SafeAreaView, Image, TextInput } from "react-native"
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from "react-native"
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from "@react-navigation/native"
 import { ChevronDownIcon, UserIcon, AdjustmentsVerticalIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline'
+import Categories from "../components/categories"
 
 
 const HomeScreen = () => {
@@ -15,9 +16,9 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt-5">
-    <Text>
+    
       {/* Header */}
-      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 ">
         <Image
           source={{
             uri: 'https://links.papareact.com/wru',
@@ -34,7 +35,7 @@ const HomeScreen = () => {
       </View>
 
       {/* Search */}
-          <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+          <View className="flex-row items-center space-x-2 pb-2 mx-4">
             <View className="flex-row flex-1 space-x-4 bg-gray-200 p-3">
             <MagnifyingGlassIcon color="gray" size={20} />
             <TextInput placeholder="Restaurants and cuisines"
@@ -45,7 +46,17 @@ const HomeScreen = () => {
             <AdjustmentsVerticalIcon color="#00CCBB"/>
           </View>
 
-    </Text>
+
+          {/* Body */}
+          <ScrollView className="bg-gray-100">
+            {/* Categories */}
+            <Categories />
+            {/* Featured Row */}
+          </ScrollView>
+
+
+
+    
     </SafeAreaView>
   )
 }
