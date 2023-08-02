c9227510e6cc1b936290d15151513c933f280f43
ElmerPC <erey2790@gmail.com>
2023-08-02 14:31:13 -0500

    added the search bar and input text box

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
-import { View, Text, SafeAreaView, Image } from "react-native"
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native"
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from "@react-navigation/native"
-import { ChevronDownIcon, UserIcon } from 'react-native-heroicons/solid'
+import { ChevronDownIcon, UserIcon, AdjustmentsVerticalIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline'
 
 
 const HomeScreen = () => {
@@ -17,7 +17,7 @@ const HomeScreen = () => {
     <SafeAreaView className="bg-white pt-5">
     <Text>
       {/* Header */}
-      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
         <Image
           source={{
             uri: 'https://links.papareact.com/wru',
@@ -32,6 +32,19 @@ const HomeScreen = () => {
 
           <UserIcon size={35} color="#00CCBB"/>
       </View>
+
+      {/* Search */}
+          <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+            <View className="flex-row flex-1 space-x-4 bg-gray-200 p-3">
+            <MagnifyingGlassIcon color="gray" size={20} />
+            <TextInput placeholder="Restaurants and cuisines"
+                       keyboardType="default"
+                        />
+            </View>
+
+            <AdjustmentsVerticalIcon color="#00CCBB"/>
+          </View>
+
     </Text>
     </SafeAreaView>
   )
