12e35082fcbc7d21d83050a3bd147c9a650c0e13
ElmerPC <erey2790@gmail.com>
2023-08-13 22:04:12 -0500

    DishRow component started, npm i currency

---
+++ components/DishRow.js
@@ -0,0 +1,15 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const DishRow = ({ id, name, description, price, image }) => {
+  return (
+    <TouchableOpacity>
+      <View>
+        <Text>{name}</Text>
+        <Text>{description}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+export default DishRow
