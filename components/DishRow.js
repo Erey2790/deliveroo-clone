d7a5e0b5d0f8361645f67efa6c8c3016afc74df2
ElmerPC <erey2790@gmail.com>
2023-08-14 12:24:58 -0500

    dish container almost finished

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,19 +1,36 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { urlFor } from "../sanity";
 
 const DishRow = ({ id, name, description, price, image }) => {
+const [ isPressed, setIsPressed ] = useState(false);
+
   return (
-    <TouchableOpacity>
-      <View>
-        <Text>{name}</Text>
-        <Text>{description}</Text>
-        <Text>
-            
-        </Text>
+    <>
+    
+    <TouchableOpacity className="bg-white border p-4 border-gray-200">
+      <View className="flex-row">
+        <View className="flex-1 pr-2">
+          <View>
+            <Text className="text-lg mb-1">{name}</Text>
+            <Text className="text-gray-400">{description}</Text>
+            <Text className="text-gray-400">${price}</Text>
+          </View>
+        </View>
+        <View>
+          <Image
+            style={{
+              borderWidth: 1,
+              borderColor: "#f3f3f4",
+            }}
+            source={{ uri: urlFor(image).url() }}
+            className="h-20 w-20 bg-gray-300 p-4 "
+          />
+        </View>
       </View>
     </TouchableOpacity>
-  )
-}
+    </>
+  );
+};
 
-export default DishRow
+export default DishRow;
