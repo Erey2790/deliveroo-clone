3b8be9afd9c0068514126b982debb3d588b1b629
ElmerPC <erey2790@gmail.com>
2023-08-13 19:48:09 -0500

    useRouter was set for new page

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,7 +1,21 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useRoute } from '@react-navigation/native'
 
 const RestaurantScreen = () => {
+  // this router thing is pretty cool cause it destructures it and does it again. 
+  const {params: {
+    id, 
+    imgUrl,
+    title,
+    rating,
+    genre,
+    address,
+    short_description,
+    dishes,
+    long,
+    lat,
+  }} = useRoute();
   return (
     <View>
       <Text>RestaurantScreen</Text>
