7d8738afcffd2afd1248e7c6112f96c10392396c
ElmerPC <erey2790@gmail.com>
2023-08-23 15:11:39 -0500

    restaurant slice added to redux store for basket icon

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useState, useMemo } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+import { selectRestaurant } from "../features/restaurantSlice";
+import { selectBasketItems } from "../features/basketSlice";
 
 const BasketScreen = () => {
+  const navigation = useNavigation();
+  const restaurant = useSelector(selectRestaurant);
+  const items = useSelector(selectBasketItems);
+  const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([])
+  const dispatch = useDispatch();
+  
+useMemo(() => {
+  const groupedItems = items.reduce(() => {
+    (results[items.id] = results[items.id] || []).push(item);
+    return results;
+  }, {});
+
+  setGroupedItemsInBasket(groupedItems)
+}, [items])
   return (
     <View>
       <Text>BasketScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default BasketScreen
+export default BasketScreen;
