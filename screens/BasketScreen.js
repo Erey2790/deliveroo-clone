148cc5d54fe7c7f3027842a0ba1b15254611c9f3
ElmerPC <erey2790@gmail.com>
2023-08-24 15:10:03 -0500

    the place order button finished

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -88,6 +88,10 @@ useMemo(() => {
           <Text>Order Total</Text>
           <Text className='font-extrabold'>{bastketTotal + 5.99}</Text>
         </View>
+
+        <TouchableOpacity className='rounded-lg bg-[#00CCBB] p-4'>
+          <Text className='text-center text-white text-lg font-bold'>Place Order</Text>
+        </TouchableOpacity>
       </View>
     </View>
    </SafeAreaView>
