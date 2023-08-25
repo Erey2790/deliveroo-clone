8e7d2ef9359075eb3fc9638e40de5d9c0d7572a6
ElmerPC <erey2790@gmail.com>
2023-08-25 11:26:11 -0500

    progress bar and animation almost complete

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,11 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import * as Animatable from 'react-native-animatable';
+
 
 const PreparingOrderScreen = () => {
   return (
-    <View>
-      <Text>PreparingOrderScreen</Text>
-    </View>
+    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center">
+      <Animatable.Image
+        source={require('../assets/deliveroodribbbble.gif')}
+        animation='slideInUp'
+        iterationCount={1}
+        className='h-96 w-96'
+      />
+      <Animatable.Text
+      animation='slideInUp'
+      iterationCount={1}
+      className='text-lg my-10 text-white font-bold text-center'
+      >Waiting for Restaurant to accept your order!
+      </Animatable.Text>  
+
+    </SafeAreaView>
   )
 }
 
